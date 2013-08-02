f65dcbfcbc2fee7add19303ef9f3f91bc67360f6
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2013-08-02 17:09:03 -0300

    Allow concurrency on development enviroment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ RailsAndServerSentEvents::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Enable more than one request
+  config.allow_concurrency = true
 end
